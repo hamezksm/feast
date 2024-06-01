9e5c0011fc653eb33e94214fdbe90e06173878bc
hamezksm <jamcav097@gmail.com>
2024-06-01 17:48:35 +0300

    Cleaned up the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,9 @@
 import 'package:feast/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Feast',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const DashBoard(),
-    );
-  }
 }
